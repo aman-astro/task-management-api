4503f8b7f53680d996cf01c3c5f6cfef28bc7c4e
aman-astro <aman@ikshalabs.com>
2025-08-05 15:55:16 +0530

    Update user_params to permit only name attribute in UsersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -83,6 +83,6 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name)
   end
 end
